3e8a79d6cb2945908151756445232b4c7d277bf0
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2024-12-16 22:32:39 +0300

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
-import express from 'express'
+import express from "express";
+import connectdb from './db.js'
 
-const app = express()
-
-
-app.get('/', (req, res)=> {
-  res.send("<h1>this is from the server</h1>")
-})
-
-app.listen(3000, ()=>{
-  console.log("server is running on port 3000")
-})
+const app = express();
 
 
-// 
+
+app.get("/", (req, res) => {
+  res.send("<h1>this is from the server</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+  connectdb();
+});
+
+
