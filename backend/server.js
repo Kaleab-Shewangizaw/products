704cc28b0c3f60e624e86c24075e173f189648ae
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2024-12-22 04:10:00 +0300

    added the port in .rnv file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import connectdb from "./db.js";
+import dotenv from 'dotenv'
 import router from "./routes/product.route.js";
 
 const app = express();
 
+dotenv.config()
 app.use(express.json());
-
+const port = process.env.PORT
 app.use('/api/products', router)
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(port, () => {
+  console.log("server is running on port " + port);
   connectdb();
 });
